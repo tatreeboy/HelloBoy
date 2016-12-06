4cdac06bf1295b9413e50045da91907f9fbc916a
tatreewo <tatreewo@NBAHP1512056.corp.ais900.org>
2016-12-06 09:55:35 +0700

    test1

--- TestProject/src/com/test/Test.java
+++ TestProject/src/com/test/Test.java
@@ -3,8 +3,8 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-
+		System.out.println("test444");
+		System.out.println("1234556");
 	}
 
 }
